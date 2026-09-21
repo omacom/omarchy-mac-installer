f8922b35e3cae68e323ba8d6fad1f30b18cfa6c5
Marcelo Alcantara <maralc@gmail.com>
2026-09-21 16:22:31 +1000

    Prepare installer 2.0.6 for publication (#195)
    Imported from maralcbr/omarchy-mx-mac@c84747dd8d31f6b3f0dcbf2c736e0f667765d275.

--- Packaging/build-app.sh
+++ Packaging/build-app.sh
@@ -31,8 +31,8 @@ build_jobs="${OMARCHY_BUILD_JOBS:-10}"
   || fail "OMARCHY_BUILD_JOBS must be a positive integer"
 export CARGO_BUILD_JOBS="$build_jobs"
 
-marketing_version="${OMARCHY_APP_VERSION:-2.0.4}"
-build_number="${OMARCHY_APP_BUILD_NUMBER:-21}"
+marketing_version="${OMARCHY_APP_VERSION:-2.0.6}"
+build_number="${OMARCHY_APP_BUILD_NUMBER:-23}"
 signing_identity="${OMARCHY_APP_SIGNING_IDENTITY:--}"
 team_identifier="${OMARCHY_TEAM_ID:-}"
 
